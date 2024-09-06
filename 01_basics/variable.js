8475edf5434568933a02f9b5ffa81271a809a5e1
A. J. A. Y <137244124+azzu0999@users.noreply.github.com>
2024-09-06 04:01:21 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 14453;
+let accountEmail = "ajay@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail="vinay@gmail.com"
+accountPassword ="6261"
+accountCity ="kareli"
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
+
