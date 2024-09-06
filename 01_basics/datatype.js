8a31ca0b258326f6b63ccf204c27f5f7d604f822
A. J. A. Y <137244124+azzu0999@users.noreply.github.com>
2024-09-06 04:01:53 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; 
+//alert(3+3) we are using nodejs not broweserw
+
+//console.log(3+3);
+
+let name = "ajay"
+let age = 19
+let isLoggedIn =false
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean = true/false
+//null => standalone value
+//undefined => 
+    //symbol unique
+
+
+// object 
+
+console.log( typeof null) //obect
+console.log(typeof undefined); //undefined
